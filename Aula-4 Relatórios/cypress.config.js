fab56a43f363f0f32023a091046937d731639f62
Kauan Butura <marciobutura@outlook.com>
2023-11-11 16:57:18 -0300

    adicionando aula 3 e 4

---
+++ Aula-4 Relatórios/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+
+  e2e: {
+    baseUrl: "http://lojaebac.ebaconline.art.br",
+    projectId: "EBAC",
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+  },
+});
